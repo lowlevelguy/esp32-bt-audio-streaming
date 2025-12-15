8b53d26c445f13e5b38af537496e3484220fa252
Hamza Kacem <hamzakacem@insat.ucar.tn>
2025-12-15 16:27:21 +0100

    BT: Should be done.

--- sketch/sketch.ino
+++ sketch/sketch.ino
@@ -3,6 +3,11 @@
 #include "webserver.h"
 #include "sd.h"
 
+extern BluetoothA2DPSource a2dp;
+extern std::vector<std::string> bt_devices;
+extern int current_device_id;
+extern bool streaming;
+
 // Global variable to indicate the current state of the program
 enum {
 	SETUP_MODE,
@@ -11,9 +16,15 @@ enum {
 
 void setup() {
 	Serial.begin(9600);
+	bt_init();
+	bt_scan();
 
 }
 
 void loop() {
-
+	Serial.println("Display Decteced devices");
+	for (auto e : bt_devices){
+			Serial.println(e);
+	}
+	delay(2000);
 }
